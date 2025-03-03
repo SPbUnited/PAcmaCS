fa67f1068aca539e02afd2c0fb6d7f6504cc484f
Arsenii Iarmolinskii <yarmolinskiyam@gmail.com>
2025-03-03 14:04:41 +0300

    patch: extracted vision processing out of while loop into Vision class

--- LARCmaCS/main.py
+++ LARCmaCS/main.py
@@ -10,6 +10,12 @@ socket = context.socket(zmq.PUSH)
 socket.connect("ipc:///tmp/serviz.sock")
 
 class Vision:
+    __field_state = {
+        "ball": None,
+        Team.BLUE.name: [None for _ in range(12)],
+        Team.YELLOW.name: [None for _ in range(12)],
+    }
+
     def __init__(self):
         self.client = GrSimClient()
         self.client.init()
@@ -34,6 +40,64 @@ class Vision:
     def set_ball(self, x, y, vx, vy):
         self.client.send_ball_replacement(BallReplacement(x=x, y=y, vx=vx, vy=vy))
 
+    def update_vision(self):
+        self.update_ball()
+        self.update_robots()
+
+    def update_ball(self):
+        detection = self.get_detection()
+        if detection.balls is not None:
+            self.__field_state["ball"] = {
+                "x": detection.balls[0].x,
+                "y": detection.balls[0].y,
+            }
+        else:
+            self.__field_state["ball"] = None
+
+    def update_robots(self):
+        for team in Team:
+            for i in range(12):
+                robot = self.get_robot(team, i)
+                if robot is not None:
+                    self.__field_state[team.name][robot.robot_id] = {
+                        "x": robot.x,
+                        "y": robot.y,
+                        "rotation": robot.orientation,
+                        "robot_id": robot.robot_id,
+                    }
+                else:
+                    self.__field_state[team.name][i] = None
+
+    def get_field_info(self):
+        field_info = []
+        if self.__field_state["ball"] is not None:
+            field_info.append({
+                "type": "ball",
+                "x": self.__field_state["ball"]["x"],
+                "y": self.__field_state["ball"]["y"],
+            })
+        for robot in self.__field_state[Team.BLUE.name]:
+            if robot is None:
+                continue
+            field_info.append({
+                "type": "robot_blu",
+                "robot_id": robot["robot_id"],
+                "x": robot["x"],
+                "y": robot["y"],
+                "rotation": robot["rotation"],
+            })
+        for robot in self.__field_state[Team.YELLOW.name]:
+            if robot is None:
+                continue
+            field_info.append({
+                "type": "robot_yel",
+                "robot_id": robot["robot_id"],
+                "x": robot["x"],
+                "y": robot["y"],
+                "rotation": robot["rotation"],
+            })
+        return field_info
+
     def close(self):
         self.client.close()
 
@@ -47,89 +111,14 @@ if __name__ == '__main__':
 
     x = 0
 
-    field_state = {
-        "ball": None,
-        "robots_blu": [None for _ in range(12)],
-        "robots_yel": [None for _ in range(12)],
-    }
-
     while True:
-        # detection: Detection = Vision.get_detection()
 
-        # # Balls
-        # for ball in detection.balls:
-        #     field_state["ball"] = {
-        #         "x": ball.x,
-        #         "y": ball.y,
-        #     }
-        # # Robots
-        # for robot in detection.robots:
-        #     field_state["robots_blu" if robot.team == Team.BLUE else "robots_yel"
-        #                 ][robot.robot_id] = {
-        #         "x": robot.x,
-        #         "y": robot.y,
-        #         "rotation": robot.orientation,
-        #     }
-
-        ball = vision.get_ball()
-        if ball:
-            field_state["ball"] = {
-                "x": ball.x,
-                "y": ball.y,
-            }
-
-        robots = vision.get_robots(Team.BLUE)
-        for robot in robots:
-            field_state["robots_blu"][robot.robot_id] = {
-                "x": robot.x,
-                "y": robot.y,
-                "rotation": robot.orientation,
-                "robot_id": robot.robot_id,
-            }
-
-        robots = vision.get_robots(Team.YELLOW)
-        for robot in robots:
-            field_state["robots_yel"][robot.robot_id] = {
-                "x": robot.x,
-                "y": robot.y,
-                "rotation": robot.orientation,
-                "robot_id": robot.robot_id,
-            }
-
-        field_info = []
-        if field_state["ball"] is not None:
-            field_info.append({
-                "type": "ball",
-                "x": field_state["ball"]["x"],
-                "y": field_state["ball"]["y"],
-            })
-        for robot in field_state["robots_blu"]:
-            if robot is None:
-                continue
-            field_info.append({
-                "type": "robot_blu",
-                "robot_id": robot["robot_id"],
-                "x": robot["x"],
-                "y": robot["y"],
-                "rotation": robot["rotation"],
-            })
-        for robot in field_state["robots_yel"]:
-            if robot is None:
-                continue
-            field_info.append({
-                "type": "robot_yel",
-                "robot_id": robot["robot_id"],
-                "x": robot["x"],
-                "y": robot["y"],
-                "rotation": robot["rotation"],
-            })
+        vision.update_vision()
+        field_info = vision.get_field_info()
 
         data = {"grsim_feed": {"data": field_info, "is_visible": True}}
 
-        # print(data)
-
         socket.send_json(data)
-
 
         data = {"zmq_feed": {"data":[
             {"type": "robot_yel", "robot_id": 14, "x": x, "y": 100, "rotation": 0},
