bf4c136ec8cb771711550050208293119ff74c6a
Arsenii Iarmolinskii <yarmolinskiyam@gmail.com>
2025-03-27 21:16:08 +0300

    wip: extract canvas in separate component
    seems like a waste of time. Should put it back in one component inside
    app.svelte

--- serviz/serviz_frontend/src/App.svelte
+++ serviz/serviz_frontend/src/App.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
-    import UI from "./lib/UI.svelte";
+    import RightPanel from "./lib/rightPanel.svelte";
+    import FieldCanvas from "./lib/FieldCanvas.svelte";
+
+    let showTop = $state(false);
+    let showRight = $state(true);
+    let showBottom = $state(false);
+    let showLeft = $state(false);
+
+    let topHeight = $state(150);
+    let rightWidth = $state(200);
+    let bottomHeight = $state(150);
+    let leftWidth = $state(150);
+
+    let canvas_window = $derived({
+            offsetLeft: showLeft ? leftWidth : 0,
+            offsetTop: showTop ? topHeight : 0,
+            offsetWidth: window.innerWidth - (showLeft ? leftWidth : 0) - (showRight ? rightWidth : 0),
+            offsetHeight: window.innerHeight - (showTop ? topHeight : 0) - (showBottom ? bottomHeight : 0),
+    });
 </script>
 
 <main>
-    <canvas id="canvas"></canvas>
-    <div class="container">
-        <div id="canvas-window" class="canvas-window"></div>
-        <UI/>
-    </div>
-    <script src="/static/scripts/socket.io.js"></script>
-    <script src="/static/scripts/script.js"></script>
+
+<FieldCanvas {canvas_window} />
+
+<RightPanel
+    --card-background-color=#ffffffcc
+    --right-width="{rightWidth}px"
+    --right-pos="{showRight ? 0 : -rightWidth}px"
+    --panel-bottom="{showBottom ? bottomHeight : 0}px"
+    --panel-top="{showTop ? topHeight : 0}px"
+    bind:width={rightWidth}
+    />
+
 </main>
 
 <style>
