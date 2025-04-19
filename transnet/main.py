2916e85eb8606fcd3c6964b1e6c01ae69dcec4f1
Arsenii Iarmolinskii <yarmolinskiyam@gmail.com>
2025-04-19 16:42:43 +0300

    wip: game controller relay test

--- transnet/main.py
+++ transnet/main.py
@@ -5,6 +5,7 @@ import time
 from common.tracker_client import TrackerClient
 from common.tracker_model import TeamColor
 from grsim.client import GrSimClient
+from common.game_controller_relay import GameControllerRelay
 
 from argparse import ArgumentParser
 import yaml
@@ -136,9 +137,14 @@ if __name__ == "__main__":
     simControl = SimControl(client=client)
     robotControl = RobotControl(client=GrSimRobotControl(client=client))
 
+    game_controller_relay = GameControllerRelay(
+        game_controller_fan_url=config["transnet"]["s_game_controller_fan_url"],
+    )
+
     time.sleep(2)
 
     tracker_client.init()
+    game_controller_relay.init()
 
     s_telemetry = context.socket(zmq.PUSH)
     s_telemetry.connect(config["ether"]["s_telemetry_url"])
