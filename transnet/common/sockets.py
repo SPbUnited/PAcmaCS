d68587287e1be50f4f267588a62729df9cbf8da0
GlazovYuri <glazov.y0@gmail.com>
2026-04-30 23:33:33 +0300

    reset sockets

--- transnet/common/sockets.py
+++ transnet/common/sockets.py
@@ -1,4 +1,3 @@
-import errno
 import socket
 import struct
 from typing import Optional
@@ -10,47 +9,22 @@ import attr
 class SocketReader:
     ip: str = "224.5.23.2"
     port: int = 10020
-    timeout: Optional[float] = 0.5
+    timeout: Optional[float] = None
     sock: socket.socket = attr.ib(init=False)
     msg_size: int = attr.ib(default=65536, init=False)
-
-    error_flag: bool = False
 
     def __attrs_post_init__(self) -> None:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.ip, self.port))
 
-        mreq = struct.pack("=4s4s", socket.inet_aton(self.ip), socket.inet_aton("0.0.0.0"))
-        try:
-            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-            self.joined = True
-        except OSError as e:
-            if e.errno != errno.EADDRNOTAVAIL:
-                raise
+        mreq = struct.pack("4sl", socket.inet_aton(self.ip), socket.INADDR_ANY)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
         self.sock.settimeout(self.timeout)
 
-    def _reconnect(self) -> None:
-        try:
-            self.sock.close()
-        except OSError:
-            pass
-        self.__attrs_post_init__()
-
     def read_package(self) -> bytes:
-        while True:
-            try:
-                ans = self.sock.recv(self.msg_size)
-                self.error_flag = False
-                return ans
-            except (socket.timeout, OSError) as e:
-                if not self.error_flag:
-                    self.error_flag = True
-                    print("err in SocketReader: ", e)
-                    print("socket_reader info: ip ", self.ip, " port ", self.port)
-                    print("reconecting...\n")
-                self._reconnect()
+        return self.sock.recv(self.msg_size)
 
 
 @attr.s(auto_attribs=True, kw_only=True)
